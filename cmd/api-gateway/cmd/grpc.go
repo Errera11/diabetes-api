c39ae7358a5cf241d1747cc1a72528b7c1a911f0
Errera11 <forvitinn@mail.ru>
2025-04-20 22:44:48 +0300

    set secure false

--- cmd/api-gateway/cmd/grpc.go
+++ cmd/api-gateway/cmd/grpc.go
@@ -145,7 +145,7 @@ func gatewayResponseModifier(ctx context.Context, response http.ResponseWriter, 
 		session.Options.Path = "/"
 		session.Options.HttpOnly = true
 		session.Options.SameSite = http.SameSiteNoneMode
-		session.Options.Secure = true
+		session.Options.Secure = false
 
 		// create a session for the user.  This session is converted by gorilla
 		// into a session cookie
